2c1202e25a2b1dcb9fceee74a324f13a1e228c3a
Pluginer_Enc <z0502281868@gmail.com>
2017-07-15 10:06:41 -0800

    Create Main.php

---
+++ src/PlayerKick/Main.php
@@ -0,0 +1,22 @@
+ <?php
+namespace Playerkick;
+ 
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener؛
+use pocketmine\event\player\PlayerKickEvent;
+pocketmine\text\TextFormat as Color;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable(){
+$this->getServer()->getPluginManager()->registerEvent($this, $this);
+ }
+
+public function onKick(PlayerkickEvent $event){
+$player = $event->getPlayer();
+$name = &event->getName();
+
+$player->setKickMessage(Color::RED."لقد تم طردك من قبل الادمن لعدم تقبل اوامر السيرفر"):
+
+ }
+  }
